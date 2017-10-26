e858cb40115bea81faff36e5f784589c24c1dfab
chibitofu <erinmoony@gmail.com>
2017-10-25 17:06:10 -0700

    Penguins can now be tapped.

--- whack_a_penguin/GameScene.swift
+++ whack_a_penguin/GameScene.swift
@@ -12,6 +12,7 @@ import GameplayKit
 class GameScene: SKScene {
     var slots = [WhackSlot]()
     var popupTime = 0.85
+    var numRounds = 0
     var gameScore: SKLabelNode!
     var score = 0 {
         didSet {
@@ -47,7 +48,35 @@ class GameScene: SKScene {
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        
+        if let touch = touches.first {
+            let location = touch.location(in: self)
+            let tappedNodes = nodes(at: location)
+            
+            for node in tappedNodes {
+                if node.name == "charFriend" {
+                    let whackSlot = node.parent!.parent as! WhackSlot
+                    if !whackSlot.isVisible { continue }
+                    if whackSlot.isHit { continue }
+                    
+                    whackSlot.charNode.xScale = 0.85
+                    whackSlot.charNode.yScale = 0.85
+                    
+                    whackSlot.hit()
+                    score += 1
+                    
+                    run(SKAction.playSoundFileNamed("whack.caf", waitForCompletion: false))
+                } else if node.name == "charEnemy" {
+                    let whackSlot = node.parent!.parent as! WhackSlot
+                    if !whackSlot.isVisible { continue }
+                    if whackSlot.isHit { continue }
+                    
+                    whackSlot.hit()
+                    score -= 5
+                    
+                    run(SKAction.playSoundFileNamed("whackBad.caf", waitForCompletion: false))
+                }
+            }
+        }
     }
     
     func createSlot(at position: CGPoint) {
@@ -79,6 +108,21 @@ class GameScene: SKScene {
         DispatchQueue.main.asyncAfter(deadline: .now() + delay) {
             [unowned self] in
             self.createEnemy()
+        }
+        
+        numRounds += 1
+        
+        if numRounds >= 30 {
+            for slot in slots {
+                slot.hide()
+            }
+            
+            let gameOver = SKSpriteNode(imageNamed: "gameOver")
+            gameOver.position = CGPoint(x: 512, y: 384)
+            gameOver.zPosition = 1
+            addChild(gameOver)
+            
+            return
         }
     }
 }
